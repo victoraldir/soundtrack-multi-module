2bfcf7708bce390abc66d86772c2fb72f49a4ef7
victoraldir <vhaldir@gmail.com>
2018-07-05 23:09:17 -0300

    Add auth server and resource for artist service

---
+++ st-auth/src/main/java/com/devquartzo/stauth/StAuth.java
@@ -0,0 +1,13 @@
+package com.devquartzo.stauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StAuth {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StAuth.class, args);
+    }
+
+}
