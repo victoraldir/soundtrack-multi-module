e475044cb15f0fe946a93957570b645cd12212e6
Victor Aldir <vhsantos@atech.com.br>
2018-07-11 21:16:46 -0300

    Revoke token flow and swagger docs for authentication server

--- st-auth/src/main/java/com/devquartzo/stauth/StAuth.java
+++ st-auth/src/main/java/com/devquartzo/stauth/StAuth.java
@@ -2,9 +2,10 @@ package com.devquartzo.stauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StAuth {
+public class StAuth extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StAuth.class, args);
--- st-auth/src/main/java/com/devquartzo/stauth/config/ResourceServerConfiguration.java
+++ st-auth/src/main/java/com/devquartzo/stauth/config/ResourceServerConfiguration.java
@@ -1,12 +1,23 @@
 package com.devquartzo.stauth.config;
 
 
+import org.hibernate.engine.jdbc.spi.JdbcServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.security.oauth2.client.token.JdbcClientTokenServices;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableResourceServer
@@ -16,6 +27,9 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     private static final String SECURED_READ_SCOPE = "#oauth2.hasScope('read')";
     private static final String SECURED_WRITE_SCOPE = "#oauth2.hasScope('write')";
     private static final String SECURED_PATTERN = "/dummy/**";
+
+    @Autowired
+    private TokenStore tokenStore;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
@@ -31,4 +45,11 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                 .anyRequest().access(SECURED_READ_SCOPE);
     }
 
+    @Primary
+    @Bean
+    public DefaultTokenServices tokenServices() {
+        final DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(tokenStore);
+        return  tokenServices;
+    }
 }
